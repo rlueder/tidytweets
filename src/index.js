c6bcbf678e0ca99085de95dd2aabb0b9f85c6e5a
Rafael Lüder <rlueder@pm.me>
2020-05-15 14:55:13 -0500

    added Twit.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// @flow
+
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import Twit from "twit";
+
+const T = new Twit({
+  consumer_key: process.env.REACT_APP_CONSUMER_KEY,
+  consumer_secret: process.env.REACT_APP_CONSUMER_SECRET,
+  access_token: process.env.REACT_APP_ACCESS_TOKEN,
+  access_token_secret: process.env.REACT_APP_ACCESS_SECRET,
+  // timeout_ms:           60000,
+  strictSSL: true,
+});
+
+T.get("followers/ids", { screen_name: "raflueder" }, (err, data, response) => {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log(data);
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
