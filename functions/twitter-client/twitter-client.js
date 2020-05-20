0128816ca9ae3753d968dd4ff83efd81e73e5fd0
Rafael Lüder <rlueder@pm.me>
2020-05-20 16:57:50 -0500

    updated twitter-client function

--- functions/twitter-client/twitter-client.js
+++ functions/twitter-client/twitter-client.js
@@ -1,6 +1,21 @@
 // @flow
 
-const twitterClient = require("./client");
+const dotenv = require("dotenv").config();
+const Twitter = require("twitter-lite");
+
+const { CONSUMER_KEY, CONSUMER_SECRET } = process.env;
+
+/**
+ * @module twitterClient
+ * @summary Request Auth Token and Secret from Twitter API
+ * @see {@link https://github.com/draftbit/twitter-lite}
+ * @see {@link https://developer.twitter.com/en/docs/basics/authentication/oauth-1-0a/obtaining-user-access-tokens}
+ */
+
+const twitterClient = new Twitter({
+  consumer_key: CONSUMER_KEY,
+  consumer_secret: CONSUMER_SECRET,
+});
 
 exports.handler = async (event, context, callback) => {
   const { endpoint, token, verifier } = event.queryStringParameters;
@@ -14,11 +29,17 @@ exports.handler = async (event, context, callback) => {
         });
         break;
       case "request_token":
-        const CALLBACK_URL = "http://localhost:8888/dashboard"; // replace CALLBACK_URL in PROD
+        // TODO refactor
+        let CALLBACK_URL;
+        switch (process.env.NODE_ENV) {
+          case "production":
+            CALLBACK_URL = "https://tidytweets.netlify.app/dashboard";
+            break;
+          default:
+            CALLBACK_URL = "http://localhost:8888/dashboard";
+        }
         response = await twitterClient.getRequestToken(CALLBACK_URL);
         break;
-      case "verify_credentials":
-        response = await twitterClient.get("account/verify_credentials");
       default:
       // no default
     }
